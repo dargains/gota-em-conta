874fb87cc06b3bbcc123b1327a7dfe30797815d4
André Dargains <andre.dargains@gmail.com>
2023-10-05 18:26:36 +0100

    refactor

--- src/components/MapItem.js
+++ src/components/MapItem.js
@@ -1,14 +1,8 @@
 import React from "react";
 import { MarkerF } from "@react-google-maps/api";
 
-function getColor(value) {
-  var hue = ((1 - value) * 120).toString(10);
-  return ["hsl(", hue, ",100%,50%)"].join("");
-}
-
-const MapItem = ({ lat, lng, text, order }) => {
-  const color = getColor(order);
-  const icon = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="56" height="24" viewBox="0 0 56 24" xml:space="preserve" fill="${color}"><rect width="56" height="24" style="stroke-width:3;stroke:rgb(0,0,0)" /></svg>`;
+const MapItem = ({ lat, lng, text, color }) => {
+  const icon = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="72" height="24" viewBox="0 0 72 24" xml:space="preserve" fill="${color}"><rect width="72" height="24" style="stroke-width:3;stroke:rgb(0,0,0)" /></svg>`;
   return <MarkerF position={{ lat, lng }} label={text} icon={icon} />;
 };
 
