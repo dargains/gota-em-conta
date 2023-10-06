bf6c62e938734bc542ac0e01b0fa49d8388870de
André Dargains <andre.dargains@vodafone.com>
2023-10-06 14:32:12 +0100

    icon size

--- src/components/MapItem.js
+++ src/components/MapItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { MarkerF } from "@react-google-maps/api";
 
 const MapItem = ({ lat, lng, text, color }) => {
-  const icon = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="72" height="24" viewBox="0 0 72 24" xml:space="preserve" fill="${color}"><rect width="72" height="24" style="stroke-width:3;stroke:rgb(0,0,0)" /></svg>`;
+  const icon = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="64" height="24" viewBox="0 0 64 24" xml:space="preserve" fill="${color}"><rect width="64" height="24" style="stroke-width:3;stroke:rgb(0,0,0)" /></svg>`;
   return <MarkerF position={{ lat, lng }} label={text} icon={icon} />;
 };
 
