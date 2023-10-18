9b2a2051a6af4549630aa9109a83754effe66ac7
André Dargains <andre.dargains@vodafone.com>
2023-10-18 08:33:45 +0100

    geolocation

---
+++ src/components/CurrentLocation.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { MarkerF } from "@react-google-maps/api";
+
+const CurrentLocation = ({ lat, lng }) => {
+  const icon = `data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 30 30" xml:space="preserve" width="30" height="30" ><circle r="10" cx="15" cy="15" fill="dodgerblue"/><circle r="14" cx="15" cy="15" fill="none" style="stroke-width: 2px; stroke: dodgerblue"/></svg>`;
+  return <MarkerF position={{ lat, lng }} icon={icon} />;
+};
+
+export default CurrentLocation;
--- src/helpers.js
+++ src/helpers.js
@@ -4,3 +4,5 @@ export const getColor = (value) => {
   var hue = ((1 - value) * 120).toString(10);
   return ["hsl(", hue, ",100%,50%)"].join("");
 };
+
+export const formatNumber = (number) => parseFloat(number.toFixed(2));
