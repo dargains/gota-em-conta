643363bdb892839fa1a5adfdb7824aebe618c533
André Dargains <andre.dargains@vodafone.com>
2023-11-09 18:57:03 +0000

    add ant design

--- src/components/SelectItem.tsx
+++ src/components/SelectItem.tsx
@@ -1,3 +1,4 @@
+import { Select } from "antd";
 import React from "react";
 
 interface ItemProps {
@@ -9,29 +10,45 @@ interface SelectItemProps {
   label: string;
   id: string;
   items: ItemProps[];
-  multiple?: boolean;
-  onSelect: ({ target }: { target: HTMLSelectElement }) => void;
+  onSelect: (value: number, name: string) => void;
+  isMultiple?: boolean;
+  isDisabled?: boolean;
 }
 
 const SelectItem = ({
   label,
   id,
   items,
-  multiple,
   onSelect,
+  isMultiple,
+  isDisabled,
 }: SelectItemProps) => {
+  const options = items.map(item => {return {value: item.Id, label: item.Descritivo}});
+  
+  const handleSelect = (item) => {
+    onSelect(item, id);
+  };
+
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <select name={id} id={id} onChange={onSelect} multiple={multiple}>
-        <option value="">Qualquer um</option>
-        {items.map((item) => (
-          <option key={item.Id} value={item.Id}>
-            {item.Descritivo}
-          </option>
-        ))}
-      </select>
-    </div>
+    <Select
+    style={{ width: '100%' }}
+    placeholder="Qualquer um"
+    onChange={handleSelect}
+    options={options}
+    {...(isMultiple && {mode: "multiple"})}
+    disabled={isDisabled}
+    />
+    // <div>
+    //   <label htmlFor={id}>{label}</label>
+    //   <select name={id} id={id} onChange={onSelect} isMultiple={isMultiple}>
+    //     <option value="">Qualquer um</option>
+    //     {items.map((item) => (
+    //       <option key={item.Id} value={item.Id}>
+    //         {item.Descritivo}
+    //       </option>
+    //     ))}
+    //   </select>
+    // </div>
   );
 };
 
